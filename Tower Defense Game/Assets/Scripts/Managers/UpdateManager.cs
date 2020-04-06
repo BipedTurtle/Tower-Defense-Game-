f9da8da9d062d27b8e64bfe3b4a8714518110d8e
James Cho <chojames500@gmail.com>
2020-04-06 15:24:16 -0500

    Folders organized and Update Manager added

---
+++ Tower Defense Game/Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomScripts.Managers
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        public static UpdateManager Instance { get; private set; }
+        private void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+
+        public event Action GlobalUpdate;
+        private void Update()
+        {
+            this.GlobalUpdate?.Invoke();
+        }
+
+        public event Action GlobalFixedUpdate;
+        private void FixedUpdate()
+        {
+            this.GlobalFixedUpdate?.Invoke();
+        }
+
+        public event Action GlobalLateUpdate;
+        private void LateUpdate()
+        {
+            this.GlobalLateUpdate?.Invoke();
+        }
+    }
+}
